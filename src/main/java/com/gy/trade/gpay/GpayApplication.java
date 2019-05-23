46ca88e2e57d54a9f2d5e5225e967b6a1261ed9e
zk555 <602281555@qq.com>
2019-05-23 17:12:39 +0800

    初始化

---
+++ src/main/java/com/gy/trade/gpay/GpayApplication.java
@@ -0,0 +1,13 @@
+package com.gy.trade.gpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GpayApplication.class, args);
+    }
+
+}
